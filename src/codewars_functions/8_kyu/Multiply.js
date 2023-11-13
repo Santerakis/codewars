ec82dde727269c9bad2f22e404326659d54c8378
sergey_acer <santerakis@gmail.com>
2023-11-14 01:34:29 +0200

    Convert a String to a Number

--- src/codewars_functions/8_kyu/Multiply.js
+++ src/codewars_functions/8_kyu/Multiply.js
@@ -1,16 +1,4 @@
-// Description:
-//     Introduction
-//
-// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
-//     Task
-//
-// Given a year, return the century it is in.
-// Examples
-//
-// 1705 --> 18
-// 1900 --> 19
-// 1601 --> 17
-// 2000 --> 20
+// This code does not execute properly. Try to figure out why.
 
 multiply = function (a, b) {
     return a * b;
@@ -39,19 +27,6 @@ function multiply(a, b){
 }
 
 let multiply = (a, b) => a * b;
-
-century=y=>Math.ceil(y/100)
-
-century=y=>--y/100+1|0
-
-function century(year) {
-    let century = 1;
-    while (year > 100){
-        year = year - 100;
-        century ++;
-    }
-    return century;
-}
 
 describe("Multiply", () => {
     it("fixed tests", () => {
