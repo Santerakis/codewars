184fb0e96379fbf767f1bbac85ca9c3d478a89aa
Sergey <santerakis@gmail.com>
2024-01-29 20:10:25 +0200

    You Can not Code Under Pressure #1

---
+++ src/codewars_functions/8_kyu/You Can't Code Under Pressure #1.js
@@ -0,0 +1,41 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+}
+
+const doubleInteger = i => 2 * i;
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i;
+}
+
+const doubleInteger = i => i*2
+
+function doubleInteger(i) {
+    return i * 2 // dupa
+}
+
+function doubleInteger(i) {
+    return i << 1;
+}
+
+function doubleInteger(i) {
+    return i*2;
+}
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return 2*i;
+}
+
+function doubleInteger(i) {
+    return i + i;
+}
+
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(doubleInteger(2), 4);
+    });
